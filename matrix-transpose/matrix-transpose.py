58aed77ae8fbf34d23cd16a091f594b135e7c396
Rohit Raj <164794013+codeefy@users.noreply.github.com>
2026-03-22 01:39:44 +0530

    Update solution for matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -4,9 +4,9 @@ def matrix_transpose(A):
     """
     Return the transpose of matrix A (swap rows and columns).
     """
-    A=np.array(A)
-    N,M = A.shape
-    A_T=np.zeros((M,N), dtype=A.dtype)
+    A=np.array(A) ## convert input A into a numpy array
+    N,M = A.shape # it will extract dimension of the matrix , where n is rows and m is column 
+    A_T=np.zeros((M,N), dtype=A.dtype) # it will create a zero matrix 
 
     for i in range(N):
         for j in range(M):
